1093c5309ba6dab73d8e1703bf4a44088000a359
codermahidul <codermahidul@gmail.com>
2022-12-06 18:02:20 +0600

    Home Page 2 Done

--- js/main.js
+++ js/main.js
@@ -91,6 +91,41 @@ $(function () {
 
 
 
+    //===TESTIMONIAL SLIDER ===
+    $('.TF__home2_testimonial_slider').slick({
+        dots: true,
+        infinite: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
 
 
 
